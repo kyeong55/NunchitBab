16bbe0f1860673e65089d7af67bc37ae12059245
Taegyeong <kyeong102@gmail.com>
2016-05-02 20:20:54 +0900

    Added Battery Status Receiver

--- app/src/main/java/com/example/taegyeong/nunchitbab/service/NotificationListener.java
+++ app/src/main/java/com/example/taegyeong/nunchitbab/service/NotificationListener.java
@@ -8,6 +8,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.BatteryManager;
 import android.os.Binder;
 import android.os.IBinder;
 import android.content.IntentFilter;
@@ -28,9 +29,10 @@ import io.realm.Realm;
 @EService
 public class NotificationListener extends NotificationListenerService {
 
+    private BroadcastReceiver ringerReceiver;
+    private BroadcastReceiver batteryReceiver;
+
     private SensorManager mSensorManager;
-    private Sensor proximitySensor;
-    private Sensor lightSensor;
     private SensorEventListener proximityListener;
     private SensorEventListener lightListener;
 
@@ -52,39 +54,20 @@ public class NotificationListener extends NotificationListenerService {
     public void onCreate() {
         // 디바이스 설정=>일반=>보안=>알림 에서 해당 앱을 ON 시 서비스 시작됨
         super.onCreate();
-        BroadcastReceiver receiver = new BroadcastReceiver(){
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                AudioManager cRinger = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-                switch (cRinger.getRingerMode()) {
-                    case AudioManager.RINGER_MODE_SILENT:
-                        Log.d("getRingerMode", "Silent");
-                        break;
-                    case AudioManager.RINGER_MODE_VIBRATE:
-                        Log.d("getRingerMode", "Vibrate");
-                        break;
-                    case AudioManager.RINGER_MODE_NORMAL:
-                        Log.d("getRingerMode", "Bell");
-                }
-            }
-        };
-        IntentFilter filter=new IntentFilter(
-                AudioManager.RINGER_MODE_CHANGED_ACTION);
-        registerReceiver(receiver,filter);
         Log.d("testing", "service created");
+        defineBroadcastReceivers();
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        proximitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-        lightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         defineSensorListeners();
-        registerSensor();
         save("nunchitbab_start", new JSONObject());
+        registerBroadcastReceiver();
+        registerSensorListener();
     }
 
     @Override
     public void onDestroy(){
         Log.d("testing", "service destroyed");
+        unregisterAll();
         save("nunchitbab_end", new JSONObject());
-        unregisterSensor();
         super.onDestroy();
     }
 
@@ -119,6 +102,66 @@ public class NotificationListener extends NotificationListenerService {
     @Override
     public StatusBarNotification[] getActiveNotifications() {
         return super.getActiveNotifications();
+    }
+
+    private void defineBroadcastReceivers() {
+        ringerReceiver = new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int ringerMode = ((AudioManager) context.getSystemService(Context.AUDIO_SERVICE)).getRingerMode();
+                switch (ringerMode) {
+                    case AudioManager.RINGER_MODE_SILENT:
+                        Log.d("getRingerMode", "Silent");
+                        break;
+                    case AudioManager.RINGER_MODE_VIBRATE:
+                        Log.d("getRingerMode", "Vibrate");
+                        break;
+                    case AudioManager.RINGER_MODE_NORMAL:
+                        Log.d("getRingerMode", "Bell");
+                }
+                JSONObject json = new JSONObject();
+                try {
+                    json.put("mode", ringerMode);
+                    save("ringer", json);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        batteryReceiver = new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int batteryStatus = intent.getIntExtra(BatteryManager.EXTRA_STATUS,BatteryManager.BATTERY_STATUS_UNKNOWN);
+                /**
+                 * BATTERY_STATUS_UNKNOWN 1
+                 * BATTERY_STATUS_CHARGING 2
+                 * BATTERY_STATUS_DISCHARGING 3
+                 * BATTERY_STATUS_NOT_CHARGING 4
+                 * BATTERY_STATUS_FULL 5
+                 * */
+                switch (batteryStatus){
+                    case BatteryManager.BATTERY_STATUS_CHARGING:
+                        Log.d("debugging", "battery charging");
+                        break;
+                    case BatteryManager.BATTERY_STATUS_DISCHARGING:
+                        Log.d("debugging", "battery discharging");
+                        break;
+                    case BatteryManager.BATTERY_STATUS_FULL:
+                        Log.d("debugging", "battery full");
+                        break;
+                    case BatteryManager.BATTERY_STATUS_NOT_CHARGING:
+                        Log.d("debugging", "battery not charging");
+                        break;
+                }
+                JSONObject json = new JSONObject();
+                try {
+                    json.put("status", batteryStatus);
+                    save("battery", json);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
     }
 
     private void defineSensorListeners(){
@@ -156,12 +199,21 @@ public class NotificationListener extends NotificationListenerService {
         };
     }
 
-    public void registerSensor(){
+    public void registerBroadcastReceiver(){
+        IntentFilter ringerFilter = new IntentFilter(AudioManager.RINGER_MODE_CHANGED_ACTION);
+        IntentFilter batteryFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(ringerReceiver,ringerFilter);
+        registerReceiver(batteryReceiver,batteryFilter);
+    }
+
+    public void registerSensorListener(){
+        Sensor proximitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        Sensor lightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         mSensorManager.registerListener(proximityListener, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
         mSensorManager.registerListener(lightListener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    public void unregisterSensor(){
+    public void unregisterAll(){
         mSensorManager.unregisterListener(proximityListener);
         mSensorManager.unregisterListener(lightListener);
     }
